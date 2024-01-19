6175c189f0126ee182ff34bd311e0ca4a9726bee
unknown <chan_hoan_b7@iecl.ntt.co.jp>
2024-01-19 12:52:31 +0900

    update

--- data_module/data_module.py
+++ data_module/data_module.py
@@ -39,4 +39,34 @@ class FFTDataModule(L.LightningDataModule):
     def test_dataloader(self) -> EVAL_DATALOADERS:
         return DataLoader(self.test_set, batch_size=self.hparams.batch_size, shuffle=False, pin_memory=True)
     
+class DefaultDataModule(L.LightningDataModule):
+    def __init__(self, dataset_path: str, batch_size: int = 1024, prefix="", postfix=""):
+        super().__init__()
+        self.save_hyperparameters()
+        
+    def setup(self, stage=None):
+        dataset_path = self.hparams.dataset_path
+
+        fft_train_data = np.load(os.path.join(dataset_path, self.hparams.prefix + "train" + self.hparams.postfix + ".npy"))
+        label_train_data = np.load(os.path.join(dataset_path, self.hparams.prefix + "train_label.npy"))
+
+        fft_val_data = np.load(os.path.join(dataset_path, self.hparams.prefix + "val" + self.hparams.postfix + ".npy"))
+        label_val_data = np.load(os.path.join(dataset_path, self.hparams.prefix + "val_label.npy"))
+
+        fft_test_data = np.load(os.path.join(dataset_path, self.hparams.prefix + "test" + self.hparams.postfix + ".npy"))
+        label_test_data = np.load(os.path.join(dataset_path, self.hparams.prefix + "test_label.npy"))
+
+        self.train_set = SimpleDataset(fft_train_data, label_train_data)
+        self.val_set = SimpleDataset(fft_val_data, label_val_data)
+        self.test_set = SimpleDataset(fft_test_data, label_test_data)
+
+    def train_dataloader(self) -> TRAIN_DATALOADERS:
+        return DataLoader(self.train_set, batch_size=self.hparams.batch_size, shuffle=True, pin_memory=True)
+    
+    def val_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.val_set, batch_size=self.hparams.batch_size, shuffle=False, pin_memory=True)
+    
+    def test_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.test_set, batch_size=self.hparams.batch_size, shuffle=False, pin_memory=True)
+    
         
