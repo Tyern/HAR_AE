8aa734178993f73f42abab4ee44feb9994889a6f
unknown <chan_hoan_b7@iecl.ntt.co.jp>
2024-02-01 14:09:34 +0900

    udpate

--- data_module/custom_dataset.py
+++ data_module/custom_dataset.py
@@ -15,3 +15,23 @@ class SimpleDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.data[idx], self.label[idx] - 1
+
+class DoubleDataset(Dataset):
+    def __init__(self, data, label, additional_data, additional_label, normalize=False):
+        super().__init__()
+        self.data = data.astype(np.float32)
+        self.label = label.astype(np.int64)
+        self.additional_data = additional_data.astype(np.float32)
+        self.additional_label = additional_label.astype(np.int64)
+        
+        assert normalize == False, "normalize is not implemented"
+        assert len(self.data) == len(self.label)
+
+        self.concat_data = np.concatenate([self.data, self.additional_data])
+        self.concat_label = np.concatenate([self.data, self.additional_data])
+
+    def __len__(self):
+        return len(self.concat_data)
+
+    def __getitem__(self, idx):
+        return self.concat_data[idx], self.concat_label[idx] - 1
--- data_module/data_module.py
+++ data_module/data_module.py
@@ -7,7 +7,8 @@ import lightning as L
 from torch.utils.data import DataLoader, Dataset
 from lightning.pytorch.utilities.types import EVAL_DATALOADERS, TRAIN_DATALOADERS
 
-from data_module.custom_dataset import SimpleDataset
+from data_module.custom_dataset import SimpleDataset, DoubleDataset
+from .sampler import build_sampler
 
 class FFTDataModule(L.LightningDataModule):
     def __init__(self, dataset_path: str, batch_size: int = 1024, ):
@@ -77,3 +78,48 @@ class DefaultDataModule(L.LightningDataModule):
         return DataLoader(self.test_set, batch_size=self.hparams.batch_size, shuffle=False, pin_memory=True)
    
         
+class ALDataModule_v1(L.LightningDataModule):
+    def __init__(self, 
+                 dataset_path: str, 
+                 batch_size: int = 1024, 
+                 prefix="", 
+                 postfix="",
+                 sampler_name:str=None):
+        
+        super().__init__()
+        self.save_hyperparameters()
+
+    def setup(self, stage=None):
+        dataset_path = self.hparams.dataset_path
+        train_val_test_10000_5classes_file = \
+            os.path.join(dataset_path, self.hparams.prefix + "train_val_test" + self.hparams.postfix + ".npz")
+
+        train_data = train_val_test_10000_5classes_file.train_data
+        val_data = train_val_test_10000_5classes_file.val_data
+        test_data = train_val_test_10000_5classes_file.test_data
+        additional_data = train_val_test_10000_5classes_file.additional_data
+
+        train_label = train_val_test_10000_5classes_file.train_label
+        val_label = train_val_test_10000_5classes_file.val_label
+        test_label = train_val_test_10000_5classes_file.test_label
+        additional_label = train_val_test_10000_5classes_file.additional_label
+
+        self.concat_set = DoubleDataset(train_data, train_label, additional_data, additional_label)
+        self.train_set = SimpleDataset(train_data, train_label)
+        self.val_set = SimpleDataset(val_data, val_label)
+        self.test_set = SimpleDataset(test_data, test_label)
+        self.additional_set = SimpleDataset(additional_data, additional_label)
+        # self.sampler = build_sampler(self.hparams.sampler_name, self.concat_set, batch_size=self.hparams.batch_size)
+
+    def train_dataloader(self) -> TRAIN_DATALOADERS:
+        return DataLoader(self.train_set, pin_memory=True)
+    
+    def val_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.val_set, batch_size=self.hparams.batch_size, shuffle=False, pin_memory=True)
+    
+    def test_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.test_set, batch_size=self.hparams.batch_size, shuffle=False, pin_memory=True)
+    
+    def predict_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.test_set, batch_size=self.hparams.batch_size, shuffle=False, pin_memory=True)
+   
