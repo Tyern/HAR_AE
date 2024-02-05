573e3abf3d0732e1dfafbf6e2eec70be488bb577
tyern <txuanhoang99@gmail.com>
2024-02-05 12:11:20 +0900

    update

--- run_nb.sh
+++
@@ -1,5 +0,0 @@
-jupyter nbconvert --to python $1
-python_file=`echo $1 | sed -e "s/.ipynb/.py/g"`
-sbatch -o .slurm/out.%a.out -p gpu --gres=gpu:4 ./job.sh python $python_file ${@:2}
-sleep 1.5
-rm $python_file
